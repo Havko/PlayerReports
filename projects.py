5c51fcb1b84e6f2037eaa3cf7f5378c13eb66089
Steven Havko <stevenhavko@gmail.com>
2015-11-18 15:44:53 -0500

    added defensive perf

--- projects.py
+++ projects.py
@@ -3,12 +3,28 @@ import csv
 games = nflgame.games(2015)
 players = nflgame.combine_game_stats(games)
 c = csv.writer(open("/rushing.csv", "wb"))
-for game in games:
-	for p in game.players.rushing().sort("rushing_yds"):
-			opp = ""
-			if p.team == game.home:
-				opp = game.away
-			else:
-				opp = game.home
-			c.writerow([str(p.team), str(p), str(p.rushing_att), str(p.rushing_yds), opp] )
+#This method outputs All rushing performance of the season and the opponents
+def all_rushing():
+	for game in games:
+		for p in game.players.rushing().sort("rushing_yds"):
+				opp = ""
+				if p.team == game.home:
+					opp = game.away
+				else:
+					opp = game.home
+				c.writerow([str(p.team), str(p), str(p.rushing_att), str(p.rushing_yds), opp] )
+				
 			
+def defense_vs_rush():
+	for game in games:
+		for p in game.players.rushing().sort("rushing_yds"):
+				opp = ""
+				yds_100 = []
+				if p.team == game.home:
+					opp = game.away
+				else:
+					opp = game.home
+				if p.rushing_yds > 100:					
+					c.writerow([str(p.team), str(p), str(p.rushing_att), str(p.rushing_yds), opp] )
+			
+defense_vs_rush()
