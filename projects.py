f454ffae7d787c5ba60bee973b643b04bfabeda1
Steven Havko <stevenhavko@gmail.com>
2015-11-20 16:53:19 -0500

    Reports are working

--- projects.py
+++ projects.py
@@ -68,11 +68,27 @@ def def_vs_receiving():
 def calc_ppcp_rb():
 		def_cp = {}
 		rush_games = {}
+		matchup = {}
+	
+		week_11 = nflgame.games(2015, week=11)
+		for game in week_11:
+			
+			for p in game.players.rushing().sort("rushing_yds"):
+				if p.team == game.home:
+					match_opp = game.away
+				else:
+					match_opp = game.home
+				if p.guess_position == 'RB':
+					matchup[p.name] = match_opp
+			
+		
 		for game in games:
 			def_cp[game.home] = 0
 			def_cp[game.away] = 0
+			
 			for p in game.players.rushing().sort("rushing_yds"):
 				rush_games[p.name] = 0
+				
 		for game in games:
 
 
@@ -88,7 +104,117 @@ def calc_ppcp_rb():
 				if p.rushing_yds > 100:
 					def_cp[unicode(opp)] += 1
 					rush_games[p.name] += 1
-		c.writerow([def_cp])
-		print def_cp
-		print rush_games
-calc_ppcp_rb()
+					
+		for team in def_cp: 
+			c.writerow([team,def_cp[team]])
+		for player in rush_games:
+			c.writerow([player,rush_games[player]])
+		
+		for m in matchup:
+			c.writerow([m, matchup[m], def_cp[matchup[m]] + rush_games[m]])
+			
+
+			
+			
+def calc_ppcp_QB():
+		def_cp = {}
+		rush_games = {}
+		matchup = {}
+	
+		week_10 = nflgame.games(2015, week=10)
+		for game in week_10:
+			
+			for p in game.players.passing().sort("passing_yds"):
+				if p.team == game.home:
+					match_opp = game.away
+				else:
+					match_opp = game.home
+				if p.guess_position == 'QB':
+					matchup[p.name] = match_opp
+			
+		
+		for game in games:
+			def_cp[game.home] = 0
+			def_cp[game.away] = 0
+			
+			for p in game.players.passing().sort("passing_yds"):
+				rush_games[p.name] = 0
+				
+		for game in games:
+
+
+
+			for p in game.players.passing().sort("passing_yds"):
+
+
+				if p.team == game.home:
+					opp = game.away
+				else:
+					opp = game.home
+
+				if p.passing_yds > 300:
+					def_cp[unicode(opp)] += 1
+					rush_games[p.name] += 1
+					
+		for team in def_cp: 
+			c.writerow([team,def_cp[team]])
+		for player in rush_games:
+			c.writerow([player,rush_games[player]])
+		
+		for m in matchup:
+			c.writerow([m, matchup[m], def_cp[matchup[m]] + rush_games[m]])
+			
+			
+			
+			
+			
+			
+			
+def calc_ppcp_WR():
+		def_cp = {}
+		rush_games = {}
+		matchup = {}
+	
+		week_10 = nflgame.games(2015, week=10)
+		for game in week_10:
+			
+			for p in game.players.receiving().sort("receiving_yds"):
+				if p.team == game.home:
+					match_opp = game.away
+				else:
+					match_opp = game.home
+				if p.guess_position == 'WR':
+					matchup[p.name] = match_opp
+			
+		
+		for game in games:
+			def_cp[game.home] = 0
+			def_cp[game.away] = 0
+			
+			for p in game.players.receiving().sort("receiving_yds"):
+				rush_games[p.name] = 0
+				
+		for game in games:
+
+
+
+			for p in game.players.receiving().sort("receiving_yds"):
+
+
+				if p.team == game.home:
+					opp = game.away
+				else:
+					opp = game.home
+
+				if p.receiving_yds > 100:
+					def_cp[unicode(opp)] += 1
+					rush_games[p.name] += 1
+					
+		for team in def_cp: 
+			c.writerow([team,def_cp[team]])
+		for player in rush_games:
+			c.writerow([player,rush_games[player]])
+		
+		for m in matchup:
+			c.writerow([m, matchup[m], def_cp[matchup[m]] + rush_games[m]])
+calc_ppcp_QB()
